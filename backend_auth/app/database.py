0ada455941d823c2a8e04e26860c014fc52a7305
sad-manYT <131177688+sad-manYT@users.noreply.github.com>
2025-03-31 09:34:36 +0700

    строка подключения отредактирована

--- backend_auth/app/database.py
+++ backend_auth/app/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mssql+pyodbc://username:password@localhost/dbname?driver=ODBC+Driver+17+for+SQL+Server"
+DATABASE_URL = 'mssql+pyodbc://sonicfsw:123456789@192.168.0.22:1433/reservation+navigation?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes'
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
